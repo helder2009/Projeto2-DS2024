bb6cff8a17673a135b2d5f8b581e2e6cf5f3af7e
helder2009 <antonio.marreiro8@aluno.ce.gov.br>
2024-08-19 16:51:24 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+// 1.Faça um Programa que mostre a mensagem "Olá mundo" na tela.
+let mensagem = prompt("Digite algo")
+alert(`mensagem`)    
+
+// 2.Faça um Programa que peça um número e então mostre a mensagem "O número informado foi [número]".
+let number = prompt("Digite um número")
+alert(`O número informado foi ${number}`)
+
+// 3.Faça um Programa que peça dois números e imprima a soma.
+let number1 = parseInt(prompt("Digite um número"))
+let number2 = parseInt(prompt("Digite outro número"))
+
+// 4.Faça um Programa que peça as 4 notas bimestrais e mostre a média.
+let notas1 = parseInt(prompt("Qual sua nota do primeiro bimestre?"))
+let notas2 = parseInt(prompt("Qual sua nota do segundo bimestre?"))
+let notas3 = parseInt(prompt("Qual sua nota do terceiro bimestre?"))
+let notas4 = parseInt(prompt("Qual sua nota do quarto bimestre?"))
+
+let media = (notas1 + notas2 + notas3 + notas4) / 4
+alert(`Sua media é ${media}`)
+
+// 5.Faça um Programa que converta metros para centímetros.
+
